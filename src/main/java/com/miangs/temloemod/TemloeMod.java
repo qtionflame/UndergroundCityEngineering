b745ad9c3b6c95a10ff78a540f5d5ac9dad63497
DouYan <2962672241@qq.com>
2020-01-12 12:36:35 +0800

    全部更新3次

--- src/main/java/com/miangs/temloemod/TemloeMod.java
+++ src/main/java/com/miangs/temloemod/TemloeMod.java
@@ -21,7 +21,7 @@ public class TemloeMod
 	public static CommonProxy proxy;
 
 
-	
+
 	public static final String MODID = "temloe";
 	public static final String NAME = "Temloe NET";
 	public static final String VERSION = "1.0";
